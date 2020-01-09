94eb14a9303206105f587e7f965ddd88243e7e0d
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2020-01-09 18:26:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,10 @@
 ################################################################
-# S Cunliffe
+# S Cunliffe - Jan 2020
 # v1.0.0 Initial configuration 
 # v1.0.1 Automated VM name & output VM name
 ################################################################
 # Module to deploy Single VM
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Licensed Materials - Property of IBM
-#
-# Copyright IBM Corp. 2017.
-#
 ################################################################
-# Define variables
-
-variable "openstack_image_name" {
-  description = "The name of the image to be used for deploy operations."
-}
-
-variable "openstack_flavor_name" {
-  description = "The name of the flavor to be used for deploy operations."
-}
-
-variable "openstack_network_name" {
-  description = "The name of the network to be used for deploy operations."
-}
-
-variable "openstack_IP_address" {
-  description = "Optional specific IPV4 Address to be used for deploy."
-}
-
-variable "image_id_username" {
-  description = "The username to SSH into image ID"
-}
-
-variable "image_id_password" {
-  description = "The password of the username to SSH into image ID"
-}
-
 
 provider "openstack" {
   insecure = true
