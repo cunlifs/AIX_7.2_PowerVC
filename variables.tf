eb5bb3236c655a9520ad5e0db3b8ef3ebea7ecb2
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2020-01-09 18:26:50 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,27 @@
+# Define variables
+
+variable "openstack_image_name" {
+  description = "The name of the image to be used for deploy operations."
+}
+
+variable "openstack_flavor_name" {
+  description = "The name of the flavor to be used for deploy operations."
+}
+
+variable "openstack_network_name" {
+  description = "The name of the network to be used for deploy operations."
+}
+
+variable "openstack_IP_address" {
+  description = "Optional specific IPV4 Address to be used for deploy."
+}
+
+variable "image_id_username" {
+  description = "The username to SSH into image ID"
+}
+
+variable "image_id_password" {
+  description = "The password of the username to SSH into image ID"
+}
+
 
